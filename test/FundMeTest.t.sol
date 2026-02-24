086924fea73d595a7ae1f7bd2d79cb40d44c6cd2
shoaib-eth <shoaib.developer69@gmail.com>
2026-02-24 11:56:19 +0530

    Foundry Tests Cheatcodes

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,10 +8,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address SHOAIB = makeAddr("SHOAIB");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployer = new DeployFundMe();
         fundMe = deployer.run();
+        vm.deal(SHOAIB, STARTING_BALANCE);
     }
 
     function testMinUscIsFiveDollars() public {
@@ -28,5 +33,17 @@ contract FundMeTest is Test {
         uint256 version = fundMe.getVersion();
         console.log("Version: ", version);
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // hey, the next line of code should revert!
+        fundMe.fund(); // sending 0 eth value which is less than the minimum 5 USD
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(SHOAIB); // next tx will be sent by SHOAIB address
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(SHOAIB);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
